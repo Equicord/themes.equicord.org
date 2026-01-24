c3454c201c1881c6e266c7ac359914ad2631acd0
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-01-24 02:58:24 -0500

    fix prod url

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV;
 
 export const DEV_SERVER = "http://localhost:4321";
-export const PRODUCION_SERVER = "themes.equicord.org";
+export const PRODUCION_SERVER = "https://themes.equicord.org";
 export const SERVER = env === "development" ? DEV_SERVER : PRODUCION_SERVER;
 export const RAW_SERVER = env === "development" ? "http://localhost:4321" : "themes.equicord.org";
